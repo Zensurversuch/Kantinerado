6fc04ed5b787b7aebe9fb5bfd3327f7144305d21
Marcel Hasselberg <hasselbergm.tin22@student.dhbw-heidenheim.de>
2024-02-20 22:49:31 +0100

    4-48-Workflow

--- Web-App/Backend/app.py
+++ Web-App/Backend/app.py
@@ -2,6 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/hello")
 def hello():
     return "Hello, World!"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
