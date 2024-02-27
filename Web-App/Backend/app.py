aff8ccd0a7ac614a3b5d17956f7bef27f1d289b4
Michael Schick <michael.schick@gmx.net>
2024-02-27 10:58:36 +0100

    52-66 Deactivated testing mode

--- Web-App/Backend/app.py
+++ Web-App/Backend/app.py
@@ -12,10 +12,10 @@ app = Flask(__name__)
 # -------------------- Environment Variables -------------------------------
 postgres_pw = os.getenv("POSTGRES_PW")
 POSTGRES_URL = f"postgresql://postgres:{postgres_pw}@database/postgres"
-jwt_secret_key = os.getenv("JWT_KEY")
+jwt_secret_key = os.getenv("JWT_SECRET_KEY")
 app.config["JWT_SECRET_KEY"] = f"{jwt_secret_key}"
 
-testing = True
+testing = False
 # --------------------------------------------------------------------------
 
 engine = create_engine(POSTGRES_URL)
