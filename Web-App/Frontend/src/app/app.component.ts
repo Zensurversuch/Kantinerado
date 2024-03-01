06de7f54e388901a456cdb292be9e1b46be18cec
Marcel Hasselberg <hasselbergm.tin22@student.dhbw-heidenheim.de>
2024-03-01 08:37:59 +0100

    51-64-Fetch-Login

--- Web-App/Frontend/src/app/app.component.ts
+++ Web-App/Frontend/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from "./header/header.component";
 import {MenuComponent} from "./menu/menu.component";
 import {CommonModule} from "@angular/common";
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, MenuComponent, CommonModule],
+  imports: [RouterOutlet, HeaderComponent, MenuComponent, CommonModule, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
