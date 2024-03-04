4e0ca537c28a26aaccb8ad8a726c8ea9b1b505c2
Michael Schick <michael.schick@gmx.net>
2024-03-04 18:51:29 +0100

    103-149 Added Api functionality to create a user as an admin

--- Web-App/Backend/app.py
+++ Web-App/Backend/app.py
@@ -75,31 +75,59 @@ def login():
     else:
         return jsonify({"msg": "Falscher Benutzername oder Passwort"}), 401
 
-
-
 @app.route('/create_user', methods=['POST'])
 def create_user():
-    if request.method == 'POST':
-        data = request.json
-        email = data.get('email')
-        password = data.get('password')
-        lastName = data.get('lastName')
-        firstName = data.get('firstName')
-        role = data.get('role')
-        allergies = data.get('allergies')
-        if not (email and password and lastName and firstName and role):
-            return jsonify({"message": "Missing required fields"}), 400
+    data = request.json
+    email = data.get('email')
+    password = data.get('password')
+    lastName = data.get('lastName')
+    firstName = data.get('firstName')
+    role = data.get('role')
+    allergies = data.get('allergies')
+    if not (email and password and lastName and firstName and role):
+        return jsonify({"message": "Missing required fields"}), 400
 
-        if user_repo.get_user_by_email(email):
-            return jsonify({"message": f"User with the email {email} already exists"}), 500
+    if role != "hungernde":
+        return jsonify({"message": f"You aren't allowed to create a user with the role: {role}"}), 403
 
-        ret_value = user_repo.create_user(email, password, lastName, firstName, role, allergies)
-        if not ret_value:   # If ret_value is empty no allergies were missing
-            return jsonify({"message": "User created successful"}), 201
-        elif ret_value:     # If ret_value contains values allergies were missing
-            return jsonify({"message": f"User created successful, but the allergies {ret_value} aren't present in the database"}), 201
-        elif ret_value == False:
-            return jsonify({"message": "Failed to create user"}), 500
+    if user_repo.get_user_by_email(email):
+        return jsonify({"message": f"User with the email {email} already exists"}), 500
+
+    ret_value = user_repo.create_user(email, password, lastName, firstName, role, allergies)
+    if not ret_value:   # If ret_value is empty no allergies were missing
+        return jsonify({"message": "User created successful"}), 201
+    elif ret_value:     # If ret_value contains values allergies were missing
+        return jsonify({"message": f"User created successful, but the allergies {ret_value} aren't present in the database"}), 201
+    elif ret_value == False:
+        return jsonify({"message": "Failed to create user"}), 500
+
+@app.route('/create_user_as_admin', methods=['POST'])
+@jwt_required()
+@permission_check(user_repo)
+def create_user_as_admin():
+    data = request.json
+    email = data.get('email')
+    password = data.get('password')
+    lastName = data.get('lastName')
+    firstName = data.get('firstName')
+    role = data.get('role')
+    allergies = data.get('allergies')
+    if not (email and password and lastName and firstName and role):
+        return jsonify({"message": "Missing required fields"}), 400
+
+    if role not in ["hungernde", "admin", "kantinenmitarbeiter"]:
+        return jsonify({"message": f"the role: {role} , doesn't exist"}), 400
+
+    if user_repo.get_user_by_email(email):
+        return jsonify({"message": f"User with the email {email} already exists"}), 500
+
+    ret_value = user_repo.create_user(email, password, lastName, firstName, role, allergies)
+    if not ret_value:   # If ret_value is empty no allergies were missing
+        return jsonify({"message": "User created successful"}), 201
+    elif ret_value:     # If ret_value contains values allergies were missing
+        return jsonify({"message": f"User created successful, but the allergies {ret_value} aren't present in the database"}), 201
+    elif ret_value == False:
+        return jsonify({"message": "Failed to create user"}), 500
 
 @app.route('/all_users')
 @jwt_required()
@@ -109,7 +137,6 @@ def all_users():
     if data:
         return jsonify(data)
     return jsonify({"message": "No users found"}), 404
-
 
 @app.route('/user_by_id/<int:user_id>')
 @jwt_required()
