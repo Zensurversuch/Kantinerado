f8e6eb8233c04112a3d1619197a5904f79b97f8b
Niklas Schmidt <92508845+Epiganda@users.noreply.github.com>
2024-03-11 15:21:23 +0100

    104-Overview Added Calendar Service to format given Days and getting standard day of week

--- Web-App/Frontend/src/app/components/calendar/calendar.component.ts
+++ Web-App/Frontend/src/app/components/calendar/calendar.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import {MatDatepickerModule} from "@angular/material/datepicker";
-import {DateRange } from '@angular/material/datepicker';
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {JsonPipe} from "@angular/common";
 import {FormControl, FormGroup, FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {provideNativeDateAdapter} from "@angular/material/core";
+import {CalendarService} from "../../service/calendar/calendar.service";
 
 @Component({
   selector: 'app-calendar',
@@ -15,13 +15,45 @@ import {provideNativeDateAdapter} from "@angular/material/core";
   styleUrl: './calendar.component.scss'
 })
 export class CalendarComponent {
-  selectedRange: DateRange<Date> | undefined;
 
-  constructor() {
-    // Initialisieren Sie die ausgewählte Range hier, wenn benötigt
-  }
   range = new FormGroup({
     start: new FormControl<Date | null>(null),
     end: new FormControl<Date | null>(null),
   });
+  formattedStart: string | null = null;
+  formattedEnd: string | null = null;
+
+  constructor(
+    private calendarService: CalendarService
+  ) {
+    this.range = this.calendarService.getDefaultRangeFormGroup();
+
+
+    this.range.get('start')?.valueChanges.subscribe((newValue: Date | null) => {
+      if (newValue) {
+        this.formattedStart = calendarService.convertToDate(newValue);
+        console.log(this.formattedStart)
+      } else {
+        this.formattedStart = null;
+      }
+    });
+
+    this.range.get('end')?.valueChanges.subscribe((newValue: Date | null) => {
+      if (newValue) {
+        this.formattedEnd = calendarService.convertToDate(newValue);
+        console.log(this.formattedEnd)
+      } else {
+        this.formattedEnd = null;
+      }
+    });
+    console.log(this.formattedStart)
+    console.log(this.formattedEnd)
+  }
+  setCurrentWeek() {
+    this.calendarService.setCurrentWeek(this.range);
+  }
+
+  setNextWeek() {
+    this.calendarService.setNextWeek(this.range);
+  }
 }
