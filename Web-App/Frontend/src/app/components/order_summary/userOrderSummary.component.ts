b69638bbfe0fe842f2ad830a09bba622220f40ba
Michael Schick <michael.schick@gmx.net>
2024-03-11 23:18:16 +0100

    97-Tried to implement calendar functions in summary

--- Web-App/Frontend/src/app/components/order_summary/userOrderSummary.component.ts
+++ Web-App/Frontend/src/app/components/order_summary/userOrderSummary.component.ts
@@ -5,6 +5,8 @@ import { environment } from '../../../environments/environment';
 import { AuthService } from '../../service/authentication/auth.service';
 import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from '../header/header.component';
+import { CalendarComponent } from '../calendar/calendar.component';
+import { CalendarService } from '../../service/calendar/calendar.service';
 
 interface OrderByDay {
   date: string;
@@ -18,8 +20,10 @@ interface OrderByDay {
   imports: [
     FormsModule,
     HeaderComponent,
-    CommonModule
+    CommonModule,
+    CalendarComponent
   ],
+  providers:[CalendarService],
   styleUrls: ['./userOrderSummary.component.scss'],
   templateUrl: './userOrderSummary.component.html'
 })
@@ -27,33 +31,16 @@ export class UserOrderSummaryComponent {
   orderSumResponse: any[] = [];
   datesCreated: string[];
   ordersByDay: OrderByDay[] = [];
-  start_date: string;
-  end_date: string;
+  start_date: string | null;
+  end_date: string | null;
+  calendarComponent: CalendarComponent;
 
-
-  constructor(private http: HttpClient, private authService: AuthService) {
-    const headers = new HttpHeaders().set('Authorization', `Bearer ${this.authService.getJwtToken()}`);
-    this.start_date = '2024-03-01';     // MUSS WENN KALENDER IMPLEMENTIERT IST ÜBER KALENDER GESETZT WERDEN
-    this.end_date = '2024-03-31';       // MUSS WENN KALENDER IMPLEMENTIERT IST ÜBER KALENDER GESETZT WERDEN
+  constructor(private http: HttpClient, private authService: AuthService, private calendarService: CalendarService) {
+    this.start_date = "2024-01-01";
+    this.end_date = "2024-12-01";
     this.datesCreated = [];
-
-    this.http.get<any[]>(`${environment.apiUrl}/orders_by_user/${this.start_date}/${this.end_date}`, { headers })
-      .subscribe(
-        (orderSumResponse) => {
-          console.log('GET request successful', orderSumResponse);
-          this.orderSumResponse = orderSumResponse;
-          this.orderSumResponse.forEach(order => {
-            if (!this.datesCreated.includes(order.mealPlanDate)) {
-              this.datesCreated.push(order.mealPlanDate);
-            }
-          });
-          this.groupOrdersByDay();
-        },
-        (error) => {
-          console.error('Error occurred:', error);
-          this.orderSumResponse = [];
-        }
-      );
+    this.calendarComponent = new CalendarComponent(this.calendarService);
+    this.getOrders();
   }
 
   groupOrdersByDay() {
@@ -79,6 +66,38 @@ export class UserOrderSummaryComponent {
 
   toggleDay(day: OrderByDay) {
     day.expanded = !day.expanded;
+  }
+
+  getOrders() {
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${this.authService.getJwtToken()}`);
+
+    this.http.get<any[]>(`${environment.apiUrl}/orders_by_user/${this.start_date}/${this.end_date}`, { headers })
+    .subscribe(
+      (orderSumResponse) => {
+        console.log('GET-Anfrage erfolgreich', orderSumResponse);
+        this.orderSumResponse = orderSumResponse;
+        this.orderSumResponse.forEach(order => {
+          if (!this.datesCreated.includes(order.mealPlanDate)) {
+            this.datesCreated.push(order.mealPlanDate);
+          }
+        });
+        this.groupOrdersByDay();
+      },
+      (error) => {
+        console.error('Fehler aufgetreten:', error);
+        this.orderSumResponse = [];
+      }
+    );
+  }
+
+  setDates() {
+    this.start_date = this.calendarComponent.getFormattedStart();
+    this.end_date = this.calendarComponent.getFormattedEnd();
+
+    console.log("START: " + this.start_date);
+    console.log("ENDE: " + this.end_date);
+
+    this.getOrders();
   }
 
 }
