659746f8f9a9a69991c8e192da30137dac2e8eb6
Niklas Schmidt <92508845+Epiganda@users.noreply.github.com>
2024-03-11 08:42:11 +0100

    104-Overview Added Basic Datepicker to DateComponent

--- Web-App/Frontend/src/app/components/calendar/calendar.component.ts
+++ Web-App/Frontend/src/app/components/calendar/calendar.component.ts
@@ -1,22 +1,27 @@
 import { Component } from '@angular/core';
-import {MatCalendar, MatDatepicker, MatDatepickerInput, MatDatepickerToggle} from "@angular/material/datepicker";
-import {MatFormField} from "@angular/material/form-field";
-import {DatePipe} from "@angular/common";
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {DateRange } from '@angular/material/datepicker';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {JsonPipe} from "@angular/common";
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {provideNativeDateAdapter} from "@angular/material/core";
 
 @Component({
   selector: 'app-calendar',
   standalone: true,
-  imports: [
-    MatDatepickerInput,
-    MatFormField,
-    MatDatepickerToggle,
-    MatDatepicker,
-    MatCalendar,
-    DatePipe
-  ],
+  imports: [MatFormFieldModule, MatDatepickerModule, FormsModule, ReactiveFormsModule, JsonPipe],
+  providers: [provideNativeDateAdapter()],
   templateUrl: './calendar.component.html',
   styleUrl: './calendar.component.scss'
 })
 export class CalendarComponent {
-  currentDate: Date | null = new Date();
+  selectedRange: DateRange<Date> | undefined;
+
+  constructor() {
+    // Initialisieren Sie die ausgewählte Range hier, wenn benötigt
+  }
+  range = new FormGroup({
+    start: new FormControl<Date | null>(null),
+    end: new FormControl<Date | null>(null),
+  });
 }
