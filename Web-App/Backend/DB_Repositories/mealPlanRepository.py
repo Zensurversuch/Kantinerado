7ca0e74cee17b90636f6cef7f9bcfbda0469f4c9
Thomas <kirchert.tin22@student.dhbw-heidenheim.de>
2024-03-14 22:37:16 +0100

    get_mealPlan now returns additional dish information and result is grouped by date

--- Web-App/Backend/DB_Repositories/mealPlanRepository.py
+++ Web-App/Backend/DB_Repositories/mealPlanRepository.py
@@ -1,10 +1,11 @@
-from sqlalchemy import Column, Integer, Date, ForeignKey, and_
+from sqlalchemy import Column, Integer, Date, ForeignKey, and_, asc
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.ext.declarative import declarative_base
-from DB_Repositories.models import MealPlan
+from DB_Repositories.models import MealPlan, Dish
 from datetime import datetime
 from random import randint
+import base64
 
 
 
@@ -58,18 +59,41 @@ class MealPlanRepository:
                 datetime.strptime(startDate, "%Y-%m-%d")
                 datetime.strptime(endDate, "%Y-%m-%d")
                 session = scoped_session(self.session_factory)
-                mealPlan = session.query(MealPlan).filter(MealPlan.date.between(startDate, endDate)).all()
-                mealPlan_list = []
+                mealPlan = session.query(MealPlan, Dish).filter(
+                    and_(
+                        MealPlan.date >= startDate,
+                        MealPlan.date <= endDate,
+                        Dish.dishID == MealPlan.dishID
+                    )).order_by(asc(MealPlan.date)).all()
 
+                grouped_mealPlans = {}
                 if mealPlan:
-                    for meal in mealPlan:
-                        meal_dict = {
+                    for meal, dish in mealPlan:
+                        meal_plan_date = meal.date
+                        dish_id = meal.dishID
+                        if meal_plan_date not in grouped_mealPlans:
+                            grouped_mealPlans[meal_plan_date] = {}
+                        if dish_id not in grouped_mealPlans [meal_plan_date]:
+                            grouped_mealPlans[meal_plan_date][dish_id] = {
                             "dishID": meal.dishID,
-                            "date": datetime.strftime(meal.date, "%Y-%m-%d")
+                            "date": datetime.strftime(meal.date, "%Y-%m-%d"),
+                            "dishName": dish.name,
+                            "dishMealType": dish.mealType,
+                            "dishPrice": dish.price,
+                            "dishingredients": dish.ingredients,
+                            "dishdietaryCategorie": dish.dietaryCategory,
+                            "dishmealType": dish.mealType,
+                            "dishimage": base64.b6dish4encode(dish.image).decode() if dish.image else None
                         }
-                        mealPlan_list.append(meal_dict)
+                    final_mealPlan_list = []
+                    for meal_plan_date, dish_info in grouped_mealPlans.items():
+                        dishes = list(dish_info.values())
+                        final_mealPlan_list.append({
+                            "mealPlanDate": meal_plan_date,
+                            "dishes": dishes
+                        })
 
-                    return True, mealPlan_list
+                    return True, final_mealPlan_list
                 return False, "mealplan not found"
             except ValueError as e:
                 return False, e
