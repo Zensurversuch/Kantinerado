40f58818cb1a631b9033fc51e43a9de120f255d6
Niklas Schmidt <92508845+Epiganda@users.noreply.github.com>
2024-03-14 21:29:05 +0100

    127-Added CreateMealPlanComponent

---
+++ Web-App/Frontend/src/app/components/meal_plan/create-meal-plan.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-meal-plan',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-meal-plan.component.html',
+  styleUrl: './create-meal-plan.component.scss'
+})
+export class CreateMealPlanComponent {
+
+}
