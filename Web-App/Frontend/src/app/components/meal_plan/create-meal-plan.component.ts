d829cd61fdec9790dc7edb1b9eec71cedbad1730
Niklas Schmidt <92508845+Epiganda@users.noreply.github.com>
2024-03-15 06:27:57 +0100

    127-Added Basic HTML and SCSS

--- Web-App/Frontend/src/app/components/meal_plan/create-meal-plan.component.ts
+++ Web-App/Frontend/src/app/components/meal_plan/create-meal-plan.component.ts
@@ -1,12 +1,58 @@
-import { Component } from '@angular/core';
+import {Component, NgIterable, Type} from '@angular/core';
+import {MatOption} from "@angular/material/autocomplete";
+import {MatFormField, MatLabel, MatSelect} from "@angular/material/select";
+import {MatCard} from "@angular/material/card";
+import {CdkDragDrop, CdkDropList} from "@angular/cdk/drag-drop";
+import {NgForOf, NgIf} from "@angular/common";
+import {HeaderComponent} from "../header/header.component";
+import {CalendarService} from "../../service/calendar/calendar.service";
+import {MealTypesArray} from "../../interface/mealType";
+import {DishData} from "../../interface/dishData";
+import {FormsModule} from "@angular/forms";
+import {DishService} from "../../service/dish/dish.service";
 
 @Component({
   selector: 'app-create-meal-plan',
   standalone: true,
-  imports: [],
+  imports: [
+    MatOption,
+    MatSelect,
+    MatLabel,
+    MatCard,
+    MatFormField,
+    CdkDropList,
+    NgForOf,
+    HeaderComponent,
+    NgIf,
+    FormsModule
+  ],
   templateUrl: './create-meal-plan.component.html',
   styleUrl: './create-meal-plan.component.scss'
 })
-export class CreateMealPlanComponent {
+export class CreateMealPlanComponent{
+  constructor(private calendarService: CalendarService, private dishService: DishService) {
 
+  }
+
+
+  weekDates= this.calendarService.getWeekDatesInTwoWeeks()
+  mealTypes: string[] = MealTypesArray;
+  dishes: (NgIterable<DishData>) | undefined;
+  selectedDish: any;
+  selectedMealType: any;
+
+  onDrop($event: CdkDragDrop<any, any>) {
+
+
+  }
+
+  loadDishes() {
+    if (this.selectedMealType) {
+      this.dishService.getDishByMealType(this.selectedMealType)
+        .subscribe((data: NgIterable<DishData> | undefined) => {
+          this.dishes = data;
+        });
+    }
+  }
 }
+
--- Web-App/Frontend/src/app/service/calendar/calendar.service.ts
+++ Web-App/Frontend/src/app/service/calendar/calendar.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {formatDate} from "@angular/common";
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {environment} from "../../../environments/environment";
@@ -9,13 +9,15 @@ import {HttpClient} from "@angular/common/http";
 })
 export class CalendarService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
+
   convertToDate(stringToFormat: Date) {
     return formatDate(stringToFormat, 'yyyy-MM-dd', 'en-US', 'GMT+1');
   }
 
   setCurrentWeek(range: FormGroup): void {
-    const url = environment.apiUrl+'/get_this_week';
+    const url = environment.apiUrl + '/get_this_week';
     this.http.get<any>(url).subscribe(data => {
       range.get('start')?.setValue(new Date(data.monday));
       range.get('end')?.setValue(new Date(data.sunday));
@@ -23,10 +25,26 @@ export class CalendarService {
   }
 
   setNextWeek(range: FormGroup): void {
-    const url = environment.apiUrl+'/get_next_week';
+    const url = environment.apiUrl + '/get_next_week';
     this.http.get<any>(url).subscribe(data => {
       range.get('start')?.setValue(new Date(data.monday));
       range.get('end')?.setValue(new Date(data.sunday));
-    });
-  }
+    });
+  }
+  getWeekDatesInTwoWeeks(): string[] {
+    const today = new Date();
+    const dayOfWeek = today.getDay();
+    const nextMonday = new Date(today);
+    nextMonday.setDate(today.getDate() - dayOfWeek + (dayOfWeek === 0 ? -6 : 1) + 14);
+    const weekDates = [];
+
+    for (let i = 0; i < 6; i++) {
+      const date = new Date(nextMonday);
+      date.setDate(nextMonday.getDate() + i);
+      const formattedDate = this.convertToDate(date);
+      weekDates.push(formattedDate);
+    }
+
+    return weekDates;
+  }
 }
