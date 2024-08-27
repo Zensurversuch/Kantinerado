3f0e574fa8518a3ee93299e38e4c225613418e1f
Schmidt-N <frickingen.niklas.schmidt+dev@protonmail.com>
2024-08-27 07:30:48 +0200

    227-239 adapted Playwright configuration for Angular

--- Web-App/Frontend/playwright.config.ts
+++ Web-App/Frontend/playwright.config.ts
@@ -15,22 +15,24 @@ export default defineConfig({
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
+  forbidOnly: !!process.env['CI'],
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env['CI'] ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env['CI'] ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    baseURL: 'http://127.0.0.1:3000',
     trace: 'on-first-retry',
   },
-
+  webServer:{
+    command: 'npx ng serve',
+    port: 4200,
+    timeout: 60 * 1000,
+    reuseExistingServer: !process.env['CI'],
+  },
   /* Configure projects for major browsers */
   projects: [
     {
@@ -68,11 +70,4 @@ export default defineConfig({
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     // },
   ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 });
