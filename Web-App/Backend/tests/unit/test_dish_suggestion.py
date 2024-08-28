e90bcaf32b60d1334e02dbd25a46f447eb8f7bf0
Thaaamy <thomaskircher2004@gmail.com>
2024-08-28 13:32:08 +0200

    251 changed tests to support auth tokens and db connection + added basic set of tests

--- Web-App/Backend/tests/unit/test_dish_suggestion.py
+++ Web-App/Backend/tests/unit/test_dish_suggestion.py
@@ -1,74 +1,96 @@
-import base64
-import json
 import pytest
-from unittest.mock import patch
+import base64
+from DB_Repositories.models import DishSuggestion
 from api_messages import get_api_messages, API_MESSAGE_DESCRIPTOR
 
-def test_create_dish_suggestion_success(client, mock_dish_suggestion_repo, mock_jwt_required, mock_user_repo, mock_get_permissions_for_role):
-    # Setze das Mock-Verhalten
-    mock_dish_suggestion_repo.create_dishSuggestion.return_value = True
-    #mock_user_repo.get_user_by_id.return_value = {'role': 'admin'}
-
-    # Erstelle die Testdaten
+@pytest.mark.usefixtures("session")
+def test_create_dish_suggestion_succes_admin(client, auth_token_admin, session):
+    """Test creating a dish suggestionas admin."""
+    # Post-Daten
     data = {
         'name': 'Test Dish',
         'ingredients': ['ingredient1', 'ingredient2'],
         'image': base64.b64encode(b'test image data').decode('utf-8')
     }
-
-    # Sende die Anfrage
     response = client.post('/create_dish_suggestion',
-                           data=json.dumps(data),
-                           content_type='application/json'
-                           #headers={'Authorization': 'Bearer test_token'}
+                           json=data,
+                           headers={'Authorization': f'Bearer {auth_token_admin}'}
                            )
-
-    # Überprüfe die Antwort
-    print("Response Data:", response.data)
-    print("Response JSON:", response.json)
+    
     assert response.status_code == 201
     assert response.json[API_MESSAGE_DESCRIPTOR] == f"{get_api_messages.SUCCESS.value}Gerichtsvorschlag erfolgreich erstellt"
-
-def test_create_dish_suggestion_missing_name(client, mock_dish_suggestion_repo, mock_jwt_required, mock_user_repo, mock_get_permissions_for_role):
-    # Setze das Mock-Verhalten
-    mock_dish_suggestion_repo.create_dishSuggestion.return_value = False
-    mock_user_repo.get_user_by_id.return_value = {'role': 'admin'}
+    
+    # Überprüfe, ob der Dish-Vorschlag tatsächlich erstellt wurde
+    suggestion = session.query(DishSuggestion).filter_by(name='Test Dish').first()
+    assert suggestion is not None
+    assert suggestion.name == 'Test Dish'
+    
+    if suggestion:
+            session.delete(suggestion)
+            session.commit()
+def test_create_dish_suggestion_kantinenmitarbeiter(client, auth_token_kantinenmitarbeiter, session):
+    """Test creating a dish suggestion as kantinenmitarbeiter."""
+    # Post-Daten
+    data = {
+        'name': 'Test Dish',
+        'ingredients': ['ingredient1', 'ingredient2'],
+        'image': base64.b64encode(b'test image data').decode('utf-8')
+    }
+    response = client.post('/create_dish_suggestion',
+                           json=data,
+                           headers={'Authorization': f'Bearer {auth_token_kantinenmitarbeiter}'}
+                           )
+    
+    assert response.status_code == 403
+    assert response.json['message'] == f"Zugriff nicht gestattet! create_dish_suggestion Berechtigung erforderlich"
+    
+    # Überprüfe, ob der Dish-Vorschlag tatsächlich erstellt wurde
+    suggestion = session.query(DishSuggestion).filter_by(name='Test Dish').first()
+    assert suggestion is None
+    
+    if suggestion:
+            session.delete(suggestion)
+            session.commit()
+            
+            
+def test_create_dish_suggestion_succes_hungernde(client, auth_token_hungernde, session):
+    """Test creating a dish suggestion as Hungernder"""
+    # Post-Daten
+    data = {
+        'name': 'Test Dish',
+        'ingredients': ['ingredient1', 'ingredient2'],
+        'image': base64.b64encode(b'test image data').decode('utf-8')
+    }
+    response = client.post('/create_dish_suggestion',
+                           json=data,
+                           headers={'Authorization': f'Bearer {auth_token_hungernde}'}
+                           )
+    
+    assert response.status_code == 201
+    assert response.json[API_MESSAGE_DESCRIPTOR] == f"{get_api_messages.SUCCESS.value}Gerichtsvorschlag erfolgreich erstellt"
+    
+    # Überprüfe, ob der Dish-Vorschlag tatsächlich erstellt wurde
+    suggestion = session.query(DishSuggestion).filter_by(name='Test Dish').first()
+    assert suggestion is not None
+    assert suggestion.name == 'Test Dish'
+    
+    if suggestion:
+            session.delete(suggestion)
+            session.commit()
+            
+def test_create_dish_suggestion_missing_name(client, auth_token_hungernde, session):
     # Testdaten ohne Namen
     data = {
         'ingredients': ['ingredient1', 'ingredient2'],
         'image': base64.b64encode(b'test image data').decode('utf-8')
     }
 
-    # Sende die Anfrage
-    response = client.post(
-        '/create_dish_suggestion',
-        data=json.dumps(data),
-        content_type='application/json',
-        headers={'Authorization': 'Bearer test_token'}
-        )
+    response = client.post('/create_dish_suggestion',
+                           json=data,
+                           headers={'Authorization': f'Bearer {auth_token_hungernde}'}
+                           )
 
     # Überprüfe die Antwort
     assert response.status_code == 400
     assert response.json[API_MESSAGE_DESCRIPTOR] == f"{get_api_messages.ERROR.value}Fülle alle erforderliche Felder aus"
 
-def test_create_dish_suggestion_failure(client, mock_dish_suggestion_repo, mock_jwt_required, mock_user_repo, mock_get_permissions_for_role):
-    # Setze das Mock-Verhalten
-    mock_dish_suggestion_repo.create_dishSuggestion.return_value = False
-    #mock_user_repo.get_user_by_id.return_value = {'role': 'admin'}
-    # Erstelle die Testdaten
-    data = {
-        'name': 'Test Dish',
-        'ingredients': ['ingredient1', 'ingredient2'],
-        'image': base64.b64encode(b'test image data').decode('utf-8')
-    }
-
-    # Sende die Anfrage
-    response = client.post('/create_dish_suggestion',
-                           data=json.dumps(data),
-                           content_type='application/json',
-                           #headers={'Authorization': 'Bearer test_token'}
-                           )
-
-    # Überprüfe die Antwort
-    assert response.status_code == 500
-    assert response.json[API_MESSAGE_DESCRIPTOR] == f"{get_api_messages.ERROR.value}Gerichtsvorschlag konnte nicht erstellt werden"
