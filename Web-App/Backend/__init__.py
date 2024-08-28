ecf1a7ea9dde3c8ff2086ceae1c337fd4a4f67cb
Thaaamy <thomaskircher2004@gmail.com>
2024-08-28 13:22:49 +0200

    251 added testing environment in form of a docker compose starting a test db and backend

--- Web-App/Backend/__init__.py
+++ Web-App/Backend/__init__.py
@@ -2,59 +2,37 @@ from flask import Flask
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from config import Config, TestingConfig, DevelopmentConfig, ProductionConfig
-from initialize_database import initialize_Postgres
+from initialize_database import initialize_Postgres, initialize_test_database
 from DB_Repositories import userRepository, dishesRepository, mealPlanRepository, orderRepository, allergyRepository, dishSuggestionRepository
 from routes_blueprints import dishSuggestion_blueprint
 
 def create_app(config_name: str = 'default') -> Flask:
     app = Flask(__name__)
-
+    
     # Konfiguration laden
     if config_name == 'testing':
         app.config.from_object(TestingConfig)
-        
-        POSTGRES_URL = app.config['SQLALCHEMY_DATABASE_URI']
-        engine = create_engine(POSTGRES_URL)
-
-        # Repositories bereitstellen
-        app.dish_suggestion_repo = dishSuggestionRepository.DishSuggestionRepository(engine)
-
-        # Initialisiere Datenbank
-        initialize_Postgres(engine)
-        
     elif config_name == 'development':
-        app.config.from_object(DevelopmentConfig)
-        # Datenbankverbindung initialisieren
-        POSTGRES_URL = app.config['SQLALCHEMY_DATABASE_URI']
-        engine = create_engine(POSTGRES_URL)
-              
-        # Repositories bereitstellen
-        app.user_repo = userRepository.UserRepository(engine)
-        app.dish_repo = dishesRepository.DishRepository(engine)
-        app.meal_plan_repo = mealPlanRepository.MealPlanRepository(engine)
-        app.order_repo = orderRepository.OrderRepository(engine)
-        app.allergy_repo = allergyRepository.AllergyRepository(engine)
-        app.dish_suggestion_repo = dishSuggestionRepository.DishSuggestionRepository(engine)
-
-        # Datenbank initialisieren
-        initialize_Postgres(engine)
+        app.config.from_object(DevelopmentConfig) 
     else:
         app.config.from_object(ProductionConfig)
-        # Datenbankverbindung initialisieren
-        POSTGRES_URL = app.config['SQLALCHEMY_DATABASE_URI']
-        engine = create_engine(POSTGRES_URL)
+
         
-        # Repositories bereitstellen
-        app.user_repo = userRepository.UserRepository(engine)
-        app.dish_repo = dishesRepository.DishRepository(engine)
-        app.meal_plan_repo = mealPlanRepository.MealPlanRepository(engine)
-        app.order_repo = orderRepository.OrderRepository(engine)
-        app.allergy_repo = allergyRepository.AllergyRepository(engine)
-        app.dish_suggestion_repo = dishSuggestionRepository.DishSuggestionRepository(engine)
-
-        # Datenbank initialisieren
+    # Datenbankverbindung initialisieren
+    POSTGRES_URL = app.config['SQLALCHEMY_DATABASE_URI']
+    engine = create_engine(POSTGRES_URL)
+              
+    # Repositories bereitstellen
+    app.user_repo = userRepository.UserRepository(engine)
+    app.dish_repo = dishesRepository.DishRepository(engine)
+    app.meal_plan_repo = mealPlanRepository.MealPlanRepository(engine)
+    app.order_repo = orderRepository.OrderRepository(engine)
+    app.allergy_repo = allergyRepository.AllergyRepository(engine)
+    app.dish_suggestion_repo = dishSuggestionRepository.DishSuggestionRepository(engine)
+    
+    if config_name != 'testing':
         initialize_Postgres(engine)
-
+    
     # Initialisiere JWT, CORS und registriere Blueprints
     from flask_jwt_extended import JWTManager
     from flask_cors import CORS
--- Web-App/Backend/config.py
+++ Web-App/Backend/config.py
@@ -6,18 +6,13 @@ class Config:
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'your_jwt_secret_key')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@database/{os.getenv('POSTGRES_DB')}"
-
-
 class DevelopmentConfig(Config):
     """Entwicklungsumgebungskonfiguration."""
     DEBUG = True
-
 class TestingConfig(Config):
     """Testumgebungskonfiguration."""
     TESTING = True
-    JWT_SECRET_KEY = 'your_test_secret_key'  
-    JWT_ACCESS_TOKEN_EXPIRES = False  
-    SERVER_NAME = 'localhost'  
+    SQLALCHEMY_DATABASE_URI = f"postgresql://postgres:test@localhost:5433/postgres_test"
 
 class ProductionConfig(Config):
     """Produktionsumgebungskonfiguration."""
