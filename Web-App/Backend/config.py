acb38fbb307e677cca52ed0256def3e082047c70
WANZL\thokir <thomas.kircher@wanzl.com>
2024-09-02 09:52:30 +0200

    251 changed name of test database container

--- Web-App/Backend/config.py
+++ Web-App/Backend/config.py
@@ -5,7 +5,7 @@ class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key')
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'your_jwt_secret_key')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@database/{os.getenv('POSTGRES_DB')}"
+    SQLALCHEMY_DATABASE_URI = f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@database-test/{os.getenv('POSTGRES_DB')}"
 class DevelopmentConfig(Config):
     """Entwicklungsumgebungskonfiguration."""
     DEBUG = True
