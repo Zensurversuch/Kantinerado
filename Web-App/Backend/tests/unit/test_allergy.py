b2f02c44216499c362e46441e2f2996fd0fda796
Thaaamy <thomaskircher2004@gmail.com>
2024-09-10 16:23:13 +0200

    Addressed feedback from PR

--- Web-App/Backend/tests/unit/test_allergy.py
+++ Web-App/Backend/tests/unit/test_allergy.py
@@ -29,7 +29,7 @@ def test_all_allergies_success(session, client):
     assert response.status_code == 200
     assert response.json == [{'allergieID': 1, 'name': 'TestAllergyOne'}, {'allergieID': 2, 'name': 'TestAllergyTwo'}]
     
-def test_all_allergies(session, client, delete_all_allergies, reset_allergies):
+def test_all_allergies_no_allergies_available(session, client, delete_all_allergies, reset_allergies):
     response = client.get(f'/all_allergies')
 
     assert response.status_code == 404
