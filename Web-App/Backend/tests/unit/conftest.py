019bf432d61f435070f0a2307e3ec764dc29cf3d
Marcel Hasselberg <marcel.hasselberg@mercedes-benz.com>
2024-09-11 00:25:50 +0200

    224-269-create-order-tests

--- Web-App/Backend/tests/unit/conftest.py
+++ Web-App/Backend/tests/unit/conftest.py
@@ -1,10 +1,11 @@
+from datetime import datetime, timedelta
 import pytest
 from flask_jwt_extended import create_access_token
 from __init__ import create_app
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from initialize_database import initialize_test_database
-from DB_Repositories.models import DishSuggestion, Dish, dish_allergy_association
+from DB_Repositories.models import DishSuggestion, Dish, MealPlan, Order, dish_allergy_association
 
 @pytest.fixture(scope='session')
 def app():
@@ -32,6 +33,15 @@ def session(app):
     session.close()
 
 @pytest.fixture(scope='function')
+def delete_all_orders_mealPlans_dishes(session):
+    """Fixture, to delete all orders, mealPlans and dishes from database."""
+    session.query(Order).delete()
+    session.query(MealPlan).delete()
+    session.query(dish_allergy_association).delete()
+    session.query(Dish).delete()
+    session.commit()
+
+@pytest.fixture(scope='function')
 def delete_all_dish_suggestions(session):
     """Fixture, to delete all dish suggestions from database."""
     session.query(DishSuggestion).delete()
@@ -40,6 +50,8 @@ def delete_all_dish_suggestions(session):
 @pytest.fixture(scope='function')
 def delete_all_dishes(session):
     """Fixture, to delete all dishes from database."""
+    session.query(Order).delete()
+    session.query(MealPlan).delete()
     session.query(dish_allergy_association).delete()
     session.query(Dish).delete()
     session.commit()
