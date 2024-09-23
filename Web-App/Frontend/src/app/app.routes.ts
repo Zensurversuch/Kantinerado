4e621745f359ee547792b46b274204c2051f5d45
Schmidt-N <dev.niklas@proton.me>
2024-09-23 15:00:29 +0200

    Hotfix: deleted import of missing 'Hello' Component

--- Web-App/Frontend/src/app/app.routes.ts
+++ Web-App/Frontend/src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import {Routes} from '@angular/router';
 import {LoginComponent} from "./components/login/login.component";
-import {HelloComponent} from "./components/hello/hello.component";
 import {HomeComponent} from "./components/home/home.component";
 import {RegisterComponent} from "./components/register/register.component";
 import {UserOrderSummaryComponent} from "./components/order_summary/userOrderSummary.component"
